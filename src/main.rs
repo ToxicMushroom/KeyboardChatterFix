815d79b2c4dfced948d8e233b9004e18f687085b
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2024-02-14 22:53:49 +0100

    Cleanup
    Co-authored-by: zeroeightysix <ridanvandenbergh@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::array::from_fn;
+#![feature(if_let_guard)]
+#![feature(let_chains)]
+
 use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{Error, Write};
 use std::path::Path;
 use std::time::{Duration, SystemTime};
+
 use evdev::{enumerate, EventType, InputEvent, InputEventKind, Key};
 use evdev::uinput::VirtualDeviceBuilder;
 use ini::ini;
@@ -46,7 +49,7 @@ async fn main() -> Result<(), Error> {
     let ini = ini!(our_config_path.as_str());
     let binding = ini["default"]["id"].clone().unwrap_or(String::new());
     let kid = binding.as_str();
-    let threshold = ini["default"]["threshold"].clone().map_or_else(|| { 30 }, |string: String| { string.parse::<u32>().unwrap() });
+    let threshold = ini["default"]["threshold"].clone().map_or(30, |string: String| { string.parse::<u32>().unwrap() });
     let threshold_dur = Duration::from_millis(threshold as u64);
 
     let (_, mut dev) = devices
@@ -58,7 +61,7 @@ async fn main() -> Result<(), Error> {
 
     let keys = dev.supported_keys().unwrap();
     let mut fake_keyboard = VirtualDeviceBuilder::new()?
-        .name("Fake Keyboard")
+        .name("Chatter Fix Emulated Keyboard")
         .with_keys(&keys)?
         .build()
         .unwrap();
@@ -68,9 +71,7 @@ async fn main() -> Result<(), Error> {
         println!("Available as {}", path.display());
     }
 
-    let mut pressed_hist: [KeyPress; 0x2e7] = from_fn(|i| {
-        KeyPress { key: Key(i as u16), time: SystemTime::UNIX_EPOCH }
-    });
+    let mut pressed_hist = [SystemTime::UNIX_EPOCH; 0x2e7];
     let mut backlog: Vec<KeyPress> = vec![];
 
     dev.grab().expect("Could not grab (take full control of) your device");
@@ -80,63 +81,59 @@ async fn main() -> Result<(), Error> {
         let current_time = SystemTime::now();
 
         let ev = if backlog.is_empty() {
-            (Some(event_stream.next_event().await.unwrap()), None)
+            Some(event_stream.next_event().await.unwrap())
         } else {
             let wait_for = (backlog[0].time + threshold_dur) // the time in the future
                 .duration_since(current_time) // duration until that time
                 .unwrap_or(Duration::ZERO); // or 0, if the 'future' is in the past
 
             select! {
-                ev = event_stream.next_event() => (Some(ev.unwrap()), None),
-                _ = tokio::time::sleep(wait_for) => (None, Some(5)),
+                ev = event_stream.next_event() => Some(ev.unwrap()),
+                _ = tokio::time::sleep(wait_for) => None,
             }
         };
 
         match ev {
-            (Some(ev), None) => {
-                match ev.kind() {
-                    InputEventKind::Key(key) => {
-                        let key_press = KeyPress { key, time: ev.timestamp() };
-                        let pressed = ev.value() == 1;
-                        let idx = key.clone().0 as usize;
+            Some(ev) if let InputEventKind::Key(key) = ev.kind() => {
+                let key_press = KeyPress { key, time: ev.timestamp() };
+                let pressed = ev.value() == 1;
+                let idx = key.code() as usize;
+                let hist = pressed_hist[idx];
+                if pressed {
+                    // Add key to press history
+                    pressed_hist[idx] = ev.timestamp();
 
-                        let hist = &pressed_hist[idx];
-                        if pressed {
-                            // Add key to press history
-                            pressed_hist[idx] = key_press;
-
-                            // Remove key from backlog
-                            let pos = backlog.iter().position(|key_press: &KeyPress| { key_press.key == key });
-                            if let Some(pos) = pos {
-                                backlog.remove(pos);
-                                println!("Chatter prevented.");
-                            }
-                        } else {
-                            let time_diff = SystemTime::now().min(hist.time);
-                            let time_diff = time_diff.duration_since(SystemTime::UNIX_EPOCH).expect("Can't convert time_diff to a duration ?");
-                            // If depressed within threshold,
-                            if time_diff < threshold_dur {
-                                // filter the release keypress and add to backlog
-                                backlog.push(key_press);
-                                continue;
-                            }
-                        }
-
-                        // Events that occurred normally (outside threshold) are just emitted
-                        fake_keyboard.emit(&[ev]).expect("Could not emit keypress");
+                    // Remove key from backlog
+                    let pos = backlog.iter().position(|key_press: &KeyPress| { key_press.key == key });
+                    if let Some(pos) = pos {
+                        backlog.remove(pos);
+                        println!("Chatter prevented.");
                     }
-                    _ => {}
+                } else {
+                    // If depressed within threshold
+                    if let Ok(diff) = SystemTime::now().duration_since(hist) && diff < threshold_dur {
+                        // filter the release keypress and add to backlog
+                        backlog.push(key_press);
+                        continue;
+                    }
+                }
+
+                // Events that occurred normally (outside threshold) are just emitted
+                if fake_keyboard.emit(&[ev]).is_err() {
+                    eprintln!("Could not emit keypress");
                 }
             },
-            (None, Some(5)) => {
-                // Find item that has been backlogged for at least 30ms
+            None => {
+                // First item that has been backlogged for at least threshold duration
                 let old_backlog_item = &backlog[0];
 
                 // Emit found item
-                fake_keyboard.emit(&[InputEvent::new(EventType::KEY, old_backlog_item.key.0, 0)]).expect("Could not emit key release");
+                if fake_keyboard.emit(&[InputEvent::new(EventType::KEY, old_backlog_item.key.code(), 0)]).is_err() {
+                    eprintln!("Could not emit key release");
+                }
                 backlog.remove(0);
             },
-            _ => unreachable!()
+            _ => {}
         };
     }
 }
